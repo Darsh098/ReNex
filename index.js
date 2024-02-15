16eb6b7f551f1220a9e516de90c841a8dbebbdc3
unknown <darshdoshi01@gmail.com>
2024-02-15 13:22:53 +0530

    Final Changes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
-const replaceFrom = "harry"
-const replaceTo = "DD"
-const preview = true
+const path = require('path');
+const replaceFrom = "New folder"
+const replaceTo = "myFolders"
+const preview = false
+const folder = __dirname
+
 try {
-    const data = fs.readdir("data", (err, data) => {
+    const data = fs.readdir(folder, (err, data) => {
         for (let index = 0; index < data.length; index++) {
             const item = data[index];
-            let newFile = "data/" + item.replaceAll(replaceFrom, replaceTo);
+            let oldFile = path.join(folder, item)
+            let newFile = path.join(folder, item.replaceAll(replaceFrom, replaceTo));
             if (!preview) {
-                fs.rename(`data/${item}`, newFile, () => {
+                fs.rename(oldFile, newFile, () => {
                     console.log("Renamed SuccessFully...");
                 })
             }
